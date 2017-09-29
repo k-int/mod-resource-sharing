b1da09fbf46bc3f37f1ae3471c6cc9c3d822e4b0
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-09-29 12:25:02 +0100

    Working on integration test

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # mod-resource-sharing
+
+
+# Database setup
+
+N.B. This project has a database dependency on postgres functionality. The test system requires
+the following setup
+
+CREATE USER folio WITH PASSWORD 'folio' SUPERUSER CREATEDB INHERIT LOGIN;
+
+CREATE DATABASE foliotest;
+GRANT ALL PRIVILEGES ON DATABASE foliotest to folio;
+
+
---
+++ mod-resource-sharing/src/integration-test/groovy/com/k_int/folio/rs/RSRequestSpec.groovy
@@ -0,0 +1,48 @@
+package com.k_int.folio.rs
+
+
+import grails.testing.mixin.integration.Integration
+import grails.transaction.*
+import static grails.web.http.HttpHeaders.*
+import static org.springframework.http.HttpStatus.*
+import spock.lang.*
+import geb.spock.*
+import grails.plugins.rest.client.RestBuilder
+
+@Integration
+@Rollback
+class RSRequestSpec extends GebSpec {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "Set up test tenant"() {
+        when:"We post a new tenant request to the OKAPI controller"
+            def resp = restBuilder().post("$baseUrl/_/tenant") {
+              header 'X-Okapi-Tenant', 'RSTestTenant'
+            }
+
+        then:"The response is correct"
+            resp.status == OK.value()
+            // resp.headers[CONTENT_TYPE] == ['application/json;charset=UTF-8']
+            // resp.json.message == 'Welcome to Grails!'
+    }
+
+    void "Destroy test tenant"() {
+        when:"We post a delete request to the OKAPI controller"
+            def resp = restBuilder().delete("$baseUrl/_/tenant") {
+              header 'X-Okapi-Tenant', 'RSTestTenant'
+            }
+
+        then:"The response is correct"
+            resp.status == OK.value()
+            // resp.headers[CONTENT_TYPE] == ['application/json;charset=UTF-8']
+    }
+
+    RestBuilder restBuilder() {
+        new RestBuilder()
+    }
+}
