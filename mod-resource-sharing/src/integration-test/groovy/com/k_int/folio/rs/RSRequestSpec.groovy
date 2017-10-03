cae14547a884fc127e7e23f79a8e494e2ce6ac62
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-10-03 12:15:51 +0100

    Added proper geb tags to new tests

--- mod-resource-sharing/src/integration-test/groovy/com/k_int/folio/rs/RSRequestSpec.groovy
+++ mod-resource-sharing/src/integration-test/groovy/com/k_int/folio/rs/RSRequestSpec.groovy
@@ -54,33 +54,40 @@ class RSRequestSpec extends GebSpec {
         'upper' | 'Upperthorpe Library'
     }
 
-    void "User Fred from Crookes Community Library wants a copy of the Terri Grief article from americal libraries mag, march/april 2017, page 16"() {
-      def request_details = [
-        itemType:'serial',
-        title:'Americal Libraries',
-        subTitle:'THE MAGAZINE OF THE AMERICAN LIBRARY ASSOCIATION',
-        volume:'48',
-        issue:'3/4',
-        issn:'0002-9769',
-        pagination:'p16',
-        titleOfArticle:'Terri Grief',
-        patronId:'1234-5678-1234-5533-4545'
-      ]
+    void "User Makes a request"() {
 
-      def resp = restBuilder().post("$baseUrl/requests") {
-        header 'X-Okapi-Tenant', 'RSTestTenant'
-        contentType 'application/json'
-        json request_details
-      }
-      System.err.println("RESPONSE:: ${resp.json}");
-      resp.status == CREATED.value()
+      when: "We submit a new request"
+        def request_details = [
+          itemType:'serial',
+          title:'Americal Libraries',
+          subTitle:'THE MAGAZINE OF THE AMERICAN LIBRARY ASSOCIATION',
+          volume:'48',
+          issue:'3/4',
+          issn:'0002-9769',
+          pagination:'p16',
+          titleOfArticle:'Terri Grief',
+          patronId:'1234-5678-1234-5533-4545'
+        ]
+
+        def resp = restBuilder().post("$baseUrl/requests") {
+          header 'X-Okapi-Tenant', 'RSTestTenant'
+          contentType 'application/json'
+          json request_details
+        }
+
+      then: "System creates a new request record"
+        System.err.println("RESPONSE:: ${resp.json}");
+        resp.status == CREATED.value()
     }
 
-    void "User fred lists his requests"() {
-      def resp = restBuilder().get("$baseUrl/requests/search?q=1234-5678-1234-5533-4545") {
-        header 'X-Okapi-Tenant', 'RSTestTenant'
-      }
-      resp.status == OK.value()
+    void "User lists their requests"() {
+      when:"We ask the system to list requests for our user"
+        def resp = restBuilder().get("$baseUrl/requests/search?q=1234-5678-1234-5533-4545") {
+          header 'X-Okapi-Tenant', 'RSTestTenant'
+        }
+
+      then: "The system responds with the request we created above"
+        resp.status == OK.value()
 
     }
 
