5806111ba0c7277f589ee86741720a640a4b8743
Ian Ibbotson <ianibbo@gmail.com>
2018-03-10 12:05:04 +0000

    updated admin controller

--- mod-resource-sharing/grails-app/controllers/com/k_int/folio/rs/AdminController.groovy
+++ mod-resource-sharing/grails-app/controllers/com/k_int/folio/rs/AdminController.groovy
@@ -1,6 +1,8 @@
 package com.k_int.folio.rs
 
 import grails.gorm.multitenancy.CurrentTenant
+import grails.gorm.multitenancy.WithoutTenant
+
 import okapi.OkapiTenantAwareController
 import grails.converters.*
 
@@ -10,6 +12,7 @@ class AdminController {
 
   def RSProfileService
 
+  @WithoutTenant
   def registerSymbol(String symbol) {
 
     def result = [ status: 'OK' ];
@@ -19,6 +22,10 @@ class AdminController {
     // @CurrentTenant will provide tenantId. The RSProfileService is also tenant aware and accesses tenantId via that path
     log.debug("AdminController::registerSymbol(${symbol}) -- ${tenantId}");
     RSProfileService.registerSymbol(symbol, tenantId);
+
+    TenantSymbolMapping.list().each { t ->
+      log.debug("Existing tenant mapping: ${t}");
+    }
 
     render result as JSON
   }
