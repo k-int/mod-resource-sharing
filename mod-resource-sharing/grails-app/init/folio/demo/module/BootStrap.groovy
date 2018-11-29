8672cca728601f147473ccab9d2a03cb21b761ae
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2018-11-29 13:47:30 +0000

    More changes including removal of rabbit.

--- mod-resource-sharing/grails-app/init/folio/demo/module/BootStrap.groovy
+++ mod-resource-sharing/grails-app/init/folio/demo/module/BootStrap.groovy
@@ -8,11 +8,11 @@ class BootStrap {
   OkapiTenantAdminService okapiTenantAdminService
 
   def init = { servletContext ->
-    log.debug("Reporting config from folio_globals.yaml: ${grailsApplication.config.testsection.message}");
-    
-    // Freshen up the schemas.
-    okapiTenantAdminService.createTenant('diku')
-    okapiTenantAdminService.freshenAllTenantSchemas()
+//    log.debug("Reporting config from folio_globals.yaml: ${grailsApplication.config.testsection.message}");
+//    
+//    // Freshen up the schemas.
+//    okapiTenantAdminService.createTenant('diku')
+//    okapiTenantAdminService.freshenAllTenantSchemas()
     
     log.debug("BootStrap::init completed");
   }
--- mod-resource-sharing/grails-app/rabbit-consumers/com/k_int/folio/rs/RSEventConsumer.groovy
+++ mod-resource-sharing/grails-app/rabbit-consumers/com/k_int/folio/rs/RSEventConsumer.groovy
@@ -1,22 +1,22 @@
-package com.k_int.folio.rs
-
-import com.budjb.rabbitmq.consumer.MessageContext
-
-class RSEventConsumer {
-
-    static rabbitConfig = [
-        queue: 'InboundMessageQueue'
-    ]
-
-    /**
-     * Handle an incoming RabbitMQ message.
-     *
-     * @param body    The converted body of the incoming message.
-     * @param context Properties of the incoming message.
-     * @return
-     */
-    def handleMessage(def body, MessageContext messageContext) {
-        // TODO: Handle messages
-        body
-    }
-}
+//package com.k_int.folio.rs
+//
+//import com.budjb.rabbitmq.consumer.MessageContext
+//
+//class RSEventConsumer {
+//
+//    static rabbitConfig = [
+//        queue: 'InboundMessageQueue'
+//    ]
+//
+//    /**
+//     * Handle an incoming RabbitMQ message.
+//     *
+//     * @param body    The converted body of the incoming message.
+//     * @param context Properties of the incoming message.
+//     * @return
+//     */
+//    def handleMessage(def body, MessageContext messageContext) {
+//        // TODO: Handle messages
+//        body
+//    }
+//}
